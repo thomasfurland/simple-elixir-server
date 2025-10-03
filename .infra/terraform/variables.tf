87dcc45c12d6df90404db6d6fb8c0a726843955a
Thomas Furland <45860355+thomasfurland@users.noreply.github.com>
2025-10-03 19:34:38 +0900

    feat(infra): redact secrets and remove defaults

--- .infra/terraform/variables.tf
+++ .infra/terraform/variables.tf
@@ -16,43 +16,36 @@ variable "zone" {
 variable "instance_name" {
   description = "The name of the VM instance."
   type        = string
-  default     = "simple-elixir-server"
 }
 
 variable "machine_type" {
   description = "The machine type of the VM instance."
   type        = string
-  default     = "e2-medium"
 }
 
 variable "image_project" {
   description = "The project for the VM image."
   type        = string
-  default     = "ubuntu-os-cloud"
 }
 
 variable "image_family" {
   description = "The family for the VM image."
   type        = string
-  default     = "ubuntu-2204-lts"
 }
 
 variable "db_instance_name" {
   description = "The name of the Cloud SQL instance."
   type        = string
-  default     = "simple-elixir-server-db"
 }
 
 variable "db_name" {
   description = "The name of the database."
   type        = string
-  default     = "simple_elixir_server_prod"
 }
 
 variable "db_user" {
   description = "The name of the database user."
   type        = string
-  default     = "simple_elixir_server"
 }
 
 variable "db_password" {
@@ -64,11 +57,9 @@ variable "db_password" {
 variable "artifact_registry_repository_id" {
   description = "The ID of the Artifact Registry repository."
   type        = string
-  default     = "simple-elixir-server-repo"
 }
 
 variable "secret_id" {
   description = "The ID of the Secret Manager secret."
   type        = string
-  default     = "simple-elixir-server-secret"
 }
