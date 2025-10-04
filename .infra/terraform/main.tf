07a154f017828badd471cc46b885018d80c028a6
Thomas Furland <45860355+thomasfurland@users.noreply.github.com>
2025-10-05 02:30:46 +0900

    add secrets for app startup

--- .infra/terraform/main.tf
+++ .infra/terraform/main.tf
@@ -94,6 +94,14 @@ resource "google_sql_database_instance" "postgres" {
 
   settings {
     tier = "db-f1-micro"
+    
+    ip_configuration {
+      ipv4_enabled = true
+      authorized_networks {
+        name  = "vm-access"
+        value = google_compute_address.static.address
+      }
+    }
   }
 
   deletion_protection = false  # we tear down after done for the day.
