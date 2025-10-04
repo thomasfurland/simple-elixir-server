f9a8ca03b8cfded75b0a25d5a67d6d750cfc16f2
Thomas Furland <45860355+thomasfurland@users.noreply.github.com>
2025-10-05 03:04:21 +0900

    change how we create gcp sa file (#12)
    * add firewall rules to terraform
    
    * add correct config and set defaults for ansible
    
    * copy sa file to vm
    
    * build on correct os
    
    * add secrets for app startup
    
    * add digest and migrations

--- .infra/terraform/main.tf
+++ .infra/terraform/main.tf
@@ -28,10 +28,49 @@ resource "google_compute_address" "static" {
   name = "simple-elixir-static-ip"
 }
 
+# firewall: allow SSH to instances with tag "allow-ssh"
+resource "google_compute_firewall" "allow_ssh_ci" {
+  name    = "allow-ssh-ci"
+  network = google_compute_network.vpc_network.name
+
+  direction   = "INGRESS"
+  priority    = 1000
+  source_ranges = ["0.0.0.0/0"] # tighten if you have fixed egress IPs
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags = ["allow-ssh"]
+}
+
+# firewall: allow HTTP traffic to Phoenix app
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http-phoenix"
+  network = google_compute_network.vpc_network.name
+
+  direction   = "INGRESS"
+  priority    = 1000
+  source_ranges = ["0.0.0.0/0"]
+  allow {
+    protocol = "tcp"
+    ports    = ["4000"]
+  }
+
+  target_tags = ["allow-http"]
+}
+
 resource "google_compute_instance" "default" {
   name         = "simple-elixir-instance"
   machine_type = "e2-micro"
   zone         = var.zone
+
+  tags = ["allow-ssh", "allow-http"]
+
+  metadata = {
+    enable-oslogin = "FALSE"
+    ssh-keys = "ubuntu:${var.deploy_ssh_public_key}"
+  }
 
   boot_disk {
     initialize_params {
@@ -55,6 +94,14 @@ resource "google_sql_database_instance" "postgres" {
 
   settings {
     tier = "db-f1-micro"
+    
+    ip_configuration {
+      ipv4_enabled = true
+      authorized_networks {
+        name  = "vm-access"
+        value = google_compute_address.static.address
+      }
+    }
   }
 
   deletion_protection = false  # we tear down after done for the day.
@@ -70,4 +117,9 @@ resource "google_sql_user" "app" {
   name     = "appuser"
   instance = google_sql_database_instance.postgres.name
   password = var.db_password
+}
+
+output "instance_ip" {
+  description = "Public IP address of the instance"
+  value       = google_compute_address.static.address
 }
