bf51562fa5b64400a21843b6f18e513f80b24842
Thomas Furland <45860355+thomasfurland@users.noreply.github.com>
2025-10-04 22:32:23 +0900

    Fix Credo nested module warnings

--- apps/simple_elixir_server/test/support/data_case.ex
+++ apps/simple_elixir_server/test/support/data_case.ex
@@ -16,6 +16,8 @@ defmodule SimpleElixirServer.DataCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       alias SimpleElixirServer.Repo
@@ -36,10 +38,8 @@ defmodule SimpleElixirServer.DataCase do
   Sets up the sandbox based on the test tags.
   """
   def setup_sandbox(tags) do
-    pid =
-      Ecto.Adapters.SQL.Sandbox.start_owner!(SimpleElixirServer.Repo, shared: not tags[:async])
-
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = Sandbox.start_owner!(SimpleElixirServer.Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
   end
 
   @doc """
