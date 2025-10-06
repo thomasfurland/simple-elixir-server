7667d5977f3ee52f974df1acd737ef98b74f56c4
Thomas Furland <45860355+thomasfurland@users.noreply.github.com>
2025-10-06 20:56:00 +0900

    Fix stream test to assert specific Enumerable.Stream implementation

--- apps/simple_elixir_server/test/simple_elixir_server/run_data_store_test.exs
+++ apps/simple_elixir_server/test/simple_elixir_server/run_data_store_test.exs
@@ -102,7 +102,7 @@ defmodule SimpleElixirServer.RunDataStoreTest do
     test "returns a stream for existing CSV file" do
       RunDataStore.write(@test_run_id, @sample_csv_data)
       assert {:ok, stream} = RunDataStore.stream(@test_run_id)
-      assert is_function(stream.reducers)
+      assert Enumerable.impl_for(stream) == Enumerable.Stream
     end
 
     test "stream returns maps with correct keys" do
